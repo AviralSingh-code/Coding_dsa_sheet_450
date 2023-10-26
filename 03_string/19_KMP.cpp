8293b6677a2dbdc6716759d0a763cd6f45e96a84
Aviral Singh <kalhansaviral@gmail.com>
2023-10-26 10:01:57 +0530

    Added Corrections

--- 03_string/19_KMP.cpp
+++ 03_string/19_KMP.cpp
@@ -22,6 +22,75 @@
 */
 
 
+/*
+    TC O(pattern.size() + fullString.size())
+    SC O(pattern.size())
+*/
+
+vector<int> prefix_func(string s)
+{
+    int n = s.length();
+
+    vector<int> pi(n,0);
+
+    for(int i = 1;i<n;i++)
+    {
+        int j = pi[i-1];
+
+        while(j > 0 && s[i] != s[j])
+        {
+            j = pi[j-1];
+        }
+
+        if(s[i] == s[j])
+        {
+            j++;
+        }
+        pi[i] = j;
+    }
+
+    return pi;
+}
+
+int main()
+{
+    string pattern = "llo";
+    vector<int> prefix = prefix_func(pattern);
+    string fullString = "helloWorld";
+
+    int pos = -1;
+    int i = 0,j = 0;
+
+    while(i < fullString.size())
+    {
+        if(fullString[i] == pattern[j])
+        {
+            i++;
+            j++;
+        }
+        else
+        {
+            if(j != 0)
+            {
+                j = prefix[j-1];
+            }
+            else 
+            {
+                i++;
+            }
+        }
+        if(j == pattern.size())
+        {
+            pos = i - pattern.size();
+            break;
+        }
+    }
+
+    cout<<pos<<endl;
+    
+    return 0;
+}
+
 
 // ----------------------------------------------------------------------------------------------------------------------- //
 /*
